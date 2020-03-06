05c6aa42167f5ede0ee8f90341bfe9235da8c6b4
joansor <sor.joan@gmail.com>
2020-03-06 16:17:02 +0100

    ajout_postcar et widget

--- category-blog.php
+++ category-blog.php
@@ -3,6 +3,20 @@
 <section class="page-wrap">
 <div class="container">
 
+<section class="row">
+
+<div class="col-lg-3">
+
+
+    <?php if(is_active_sidebar('blog-sidebar')):?>
+
+
+    <?php dynamic_sidebar('blog-sidebar');?>
+
+
+    <?php endif;?>
+
+</div>
 
     <h1><?php echo single_cat_title();?></h1>
 
--- includes/section-cars.php
+++ includes/section-cars.php
@@ -1,18 +1,10 @@
 <?php if( have_posts() ) : while( have_posts() ) : the_post(); ?>
     
-<p><?php echo get_the_date('d/m/Y h:i:s');?></p>
+
 
     <?php the_content(); ?>
 
-    <!-- <?php the_author();?> -->
-    <?php
-    $fname = get_the_author_meta('first_name');
     
-    $lname = get_the_author_meta('last_name');
-    ?>
-
-   <p> Posted by <?php echo $fname;?> <?php echo $lname;?></p>
-
    <?php
    $tags = get_the_tags();
 
@@ -40,6 +32,10 @@ foreach($categories as $cat):?>
 
 
 <?php endforeach;?>
+
+
+<p><?php echo get_the_date('d/m/Y h:i:s');?></p>
+
 
 <?php //comments_template();?>
 
